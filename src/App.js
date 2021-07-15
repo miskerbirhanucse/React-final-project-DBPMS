49565b41ba2dba4d45284614d9691dff052bfa27
Misker Birhanu <miskerbirhanu@gmail.com>
2021-07-16 01:37:42 +0300

    some add

--- src/App.js
+++ src/App.js
@@ -8,16 +8,6 @@ import { BrowserRouter } from 'react-router-dom';
 import Routes from './Routes'
 function App() {
   return (
-<<<<<<< HEAD
-    <div >
-      {/* class="wrapper" */}
-      {/* <Header />
-      <Menu />
-      <Dashboard /> */}
-      <LoginComponent />
-      
-    </div>
-=======
     <BrowserRouter>
       <div className="App">
         {/* class="wrapper" */}
@@ -25,7 +15,6 @@ function App() {
         <Routes />
       </div>
     </BrowserRouter>
->>>>>>> f5b334d719ad391260d7dd0509e4303da5e9d8d1
   );
 }
 
