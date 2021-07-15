f5b334d719ad391260d7dd0509e4303da5e9d8d1
Misker Birhanu <miskerbirhanu@gmail.com>
2021-07-15 23:04:53 +0300

    last commit

--- src/App.js
+++ src/App.js
@@ -11,9 +11,7 @@ function App() {
     <BrowserRouter>
       <div className="App">
         {/* class="wrapper" */}
-        {/* <Header />
-      <Menu />
-      <Dashboard /> */}
+       
         <Routes />
       </div>
     </BrowserRouter>
